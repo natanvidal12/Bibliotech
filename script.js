761f509ce0cd6d936d46ba2309dc039d1aa6180b
Natan Vidal <natanvidal0313131@gmail.com>
2025-09-08 21:02:19 -0300

    Header

---
+++ script.js
@@ -0,0 +1,18 @@
+class livros{
+    constructor(id, titulo, autor, editora, numeroPaginas, genero, sinopse){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.numeroPaginas = numeroPaginas;
+        this.genero = genero;
+        this.sinopse = sinopse;
+    }
+}
+
+const Livros = [
+    new Livro = (
+        1,
+        "48 Leis do Poder"
+    )
+]
